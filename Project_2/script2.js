317bf631a0258dd5505621c2e32613f6f46e5be5
heather <hnelson@mit.edu>
2018-03-07 23:17:05 -0500

    deleting unecessary things
    in hopes that things will start working?

--- Project_2/script2.js
+++
@@ -1,9 +0,0 @@
-d3.json("../data/boston_weather.json",draw);
-
-function draw(error,data){
-    console.log(data);
-    var daily = data.daily;
-    icon = daily.data[6].icon;
-    temp = daily.data[6].apparentTemperatureHigh;
-    temp = Math.round(temp);
-}
